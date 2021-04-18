a50c58e297503c8f77360f3032a14317d995704a
kenshin579 <kenshin579@hotmail.com>
2021-04-18 15:28:50 +0900

    [BKA-1] chap4 init

---
+++ chap4-producer/src/main/java/kr/pe/advenoh/Chap4ProducerApplication.java
@@ -0,0 +1,13 @@
+package kr.pe.advenoh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chap4ProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chap4ProducerApplication.class, args);
+    }
+
+}
