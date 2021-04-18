7fa65cef827c0241dfa69c126eb82d0c016a34a5
kenshin579 <kenshin579@hotmail.com>
2021-04-18 15:48:01 +0900

    [BKA-1] chap8 init

---
+++ chap8-stream/src/main/java/kr/pe/advenoh/Chap8StreamApplication.java
@@ -0,0 +1,13 @@
+package kr.pe.advenoh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chap8StreamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chap8StreamApplication.class, args);
+    }
+
+}
