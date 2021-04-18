155a3ecce192c7f2a7ed620c65ceb73c79608763
kenshin579 <kenshin579@hotmail.com>
2021-04-18 15:43:43 +0900

    [BKA-1] chap5로 파일 옮기

--- chap5-consumer/src/main/java/kr/pe/advenoh/KafkaBookConsumerPart.java
+++ chap5-consumer/src/main/java/kr/pe/advenoh/KafkaBookConsumerPart.java
@@ -1,14 +1,25 @@
-public class KafkaBookConsumerMO {
+package kr.pe.advenoh;
+
+import org.apache.kafka.clients.consumer.*;
+import org.apache.kafka.common.TopicPartition;
+
+import java.util.Arrays;
+import java.util.Properties;
+
+public class KafkaBookConsumerPart {
   public static void main(String[] args) {
     Properties props = new Properties();
     props.put("bootstrap.servers", "peter-kafka001:9092,peter-kafka002:9092,peter-kafka003:9092");
-    props.put("group.id", "peter-manual");
+    props.put("group.id", "peter-partition");
     props.put("enable.auto.commit", "false");
     props.put("auto.offset.reset", "latest");
     props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
     props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
     KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
-    consumer.subscribe(Arrays.asList("peter-topic"));
+    String topic = "peter-topic";
+    TopicPartition partition0 = new TopicPartition(topic, 0);
+    TopicPartition partition1 = new TopicPartition(topic, 1);
+    consumer.assign(Arrays.asList(partition0, partition1));
     while (true) {
       ConsumerRecords<String, String> records = consumer.poll(100);
       for (ConsumerRecord<String, String> record : records)
